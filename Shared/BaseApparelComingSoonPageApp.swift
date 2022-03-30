6f92fd8cb07e69966b7c591e115ceb8d9bc8a989
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-31 08:32:18 +0900

    Initial Commit

---
+++ Shared/BaseApparelComingSoonPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseApparelComingSoonPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct BaseApparelComingSoonPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
